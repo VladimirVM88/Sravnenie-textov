409a7c9c176c3a8cdd443ef28ba502e59e3eeb38
VladimirVM88 <122440514+VladimirVM88@users.noreply.github.com>
2023-05-02 21:40:47 +0700

    Update lesson03_сравнение текстов.py
    Git

--- lesson03_сравнение текстов.py
+++ lesson03_сравнение текстов.py
@@ -1,15 +1,15 @@
 
-r = open('text3.txt', encoding='utf-8')
+r = open('text1.txt', encoding='utf-8') # Первый текст помещается в файл text1.txt в корне папки с программой
 r1 = set(r.read().split())
 r.close()
 
-r = open('text2.txt', encoding='utf-8')
+r = open('text2.txt', encoding='utf-8') # Второй текст помещается в файл text2.txt в корне папки с программой
 r2 = set(r.read().split())
 r.close()
 
 
-#new = r1.intersection(r2)
-new = r2.difference(r1)
-
+# new = r1.intersection(r2)  # Раскомментировать строку, если необходимо сравнить тексты на предмет использования одинаковых слов в обоих текстах
+# new = r2.difference(r1) # Показывает слова, которые не используются в r1
+new = r1.difference(r2) # Показывает слова, которые не используются в r2
         
 print(new)
